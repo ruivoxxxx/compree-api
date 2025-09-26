37aa4ee2fd0aa3f5af66163e6c179e15b40d9ee7
Junior Tavares <contatoojuniortavares@gmail.com>
2025-09-26 20:18:42 -0300

    feaat:adding filter for get produtos where deleted_at is null

--- src/produto/services/getProdutos/repository/getProdutos.repository.ts
+++ src/produto/services/getProdutos/repository/getProdutos.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ProdutoEntity } from 'src/produto/entity/produto.entity';
-import { Repository } from 'typeorm';
+import { IsNull, Repository } from 'typeorm';
 import { GetProdutosOutPutDto } from '../dto/getProdutosOutPut.dto';
 
 @Injectable()
@@ -14,6 +14,7 @@ export class GetProdutosRepository {
     async getProdutos(): Promise<GetProdutosOutPutDto[]> {
         return await this.dataBaseService.find({
             select: ['id', 'nome', 'valor', 'categoria', 'descricao'],
+            where: { deleted_at: IsNull() },
         });
     }
 }
