09ec8b197dbcd9ca6ca164c9f40fc6774e2d806f
Junior Tavares <contatoojuniortavares@gmail.com>
2025-09-26 20:17:59 -0300

    feat:create file for create produto

---
+++ src/produto/services/postProduto/repository/postProdutos.repository.ts
@@ -0,0 +1,36 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProdutoEntity } from 'src/produto/entity/produto.entity';
+import { IsNull, Repository } from 'typeorm';
+import { PostProdutoInputDto } from '../dto/postProdutosInputDto';
+import { Injectable } from '@nestjs/common';
+@Injectable()
+export class PostProdutoRepository {
+    constructor(
+        @InjectRepository(ProdutoEntity)
+        private readonly dataBaseService: Repository<ProdutoEntity>,
+    ) {}
+
+    async verificaProduto(data: PostProdutoInputDto) {
+        return await this.dataBaseService.findOne({
+            select: ['nome'],
+            where: { nome: data.nome, deleted_at: IsNull() },
+        });
+    }
+
+    async criaProduto(data: PostProdutoInputDto) {
+        await this.dataBaseService
+            .createQueryBuilder()
+            .insert()
+            .into(ProdutoEntity)
+            .values({
+                nome: () => 'UPPER(:nome)',
+                valor: data.valor,
+                quantidade: data.quantidade,
+                descricao: data.descricao,
+                categoria: data.categoria,
+                id_usuario: data.id_usuario,
+            })
+            .setParameters({ nome: data.nome })
+            .execute();
+    }
+}
