a0897b9324c1bec988726617dd0b984be4fba2f0
José Amilton Tavares Freire Júnior <amilton.freire@icc.org.br>
2025-10-08 12:56:11 -0300

    refactor:adding new names for files

--- src/pedidos/services/getPedido/dto/getPedidoInputDto.ts
+++ src/pedidos/services/getPedido/dto/getPedidoInputDto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
-export class GetPedidoByIdInputDto {
+export class GetPedidoInputDto {
     @ApiProperty()
     @IsOptional()
     @IsString()
--- src/pedidos/services/getPedido/repository/getPedido.repository.ts
+++ src/pedidos/services/getPedido/repository/getPedido.repository.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PedidosEntity } from 'src/pedidos/entity/pedido.entity';
 import { IsNull, Repository } from 'typeorm';
-import { GetPedidoByIdInputDto } from '../dto/getProdutoByIdInputDto';
+import { GetPedidoInputDto } from '../dto/getPedidoInputDto';
 
 @Injectable()
-export class GetPedidoByIdRepository {
+export class GetPedidoRepository {
     constructor(
         @InjectRepository(PedidosEntity)
         private readonly dataBaseService: Repository<PedidosEntity>,
     ) {}
 
-    async buscaProduto(data: GetPedidoByIdInputDto) {
+    async buscaProduto(data: GetPedidoInputDto) {
         const result = await this.dataBaseService.find({
             where: data.id_produto
                 ? { id: data.id_produto, deleted_at: IsNull() }
