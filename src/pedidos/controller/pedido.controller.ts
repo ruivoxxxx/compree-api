b1538849272fc2e62a6c6ed44155f689b8157d49
José Amilton Tavares Freire Júnior <amilton.freire@icc.org.br>
2025-10-09 13:55:49 -0300

    refactor:rename file in file controller

--- src/pedidos/controller/pedido.controller.ts
+++ src/pedidos/controller/pedido.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Get, Post, Query } from '@nestjs/common';
 import { PedidoService } from '../services/postPedido/service/postPedido.service';
 import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
-import { GetPedidoByIdService } from '../services/getPedidoById/service/getPedidoById.service';
-import { GetPedidoByIdInputDto } from '../services/getPedidoById/dto/getProdutoByIdInputDto';
+import { GetPedidoService } from '../services/getPedido/service/getPedido.service';
+import { GetPedidoInputDto } from '../services/getPedido/dto/getPedidoInputDto';
 
 @Controller('pedidos')
 export class PedidoController {
     constructor(
         private readonly pedidoService: PedidoService,
-        private readonly getPedidoByIdService: GetPedidoByIdService,
+        private readonly getPedidoService: GetPedidoService,
     ) {}
 
     @Post('')
@@ -21,7 +21,7 @@ export class PedidoController {
     @Get('')
     @ApiOperation({ summary: 'Busca de pedido por Id' })
     @ApiOkResponse({ description: 'Pedido encontrado com sucessopo!' })
-    async buscaProduto(@Query() data: GetPedidoByIdInputDto) {
-        return await this.getPedidoByIdService.execute(data);
+    async buscaProduto(@Query() data: GetPedidoInputDto) {
+        return await this.getPedidoService.execute(data);
     }
 }
